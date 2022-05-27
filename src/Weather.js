d8b5702caa432cc0e62b030c83961ae5df8e446b
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-05-27 14:51:27 +0100

    removed individual components

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,55 @@
 import "./Weather.css";
-import Search from "./Search";
-import Location from "./Location";
-import Time from "./Time";
-import Description from "./Description";
-import Today from "./Today";
 
 export default function Weather() {
   return (
     <div className="Weather">
       <div className="weather-app">
-        <Search />
-        <Location />
-        <Time />
-        <Description />
-        <Today />
+        <form>
+          <div className="row">
+            <div className="col float-start">
+              <button type="button" className="btn current">
+                Current
+              </button>
+            </div>
+            <div className="col-6">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Enter a location..."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col">
+              <input className="form-control search-button" type="submit" />
+            </div>
+          </div>
+        </form>
+        <h1>London</h1>
+        <h2>Last updated: Friday 2:00 </h2>
+
+        <div className="col float-end">
+          <ul>
+            <li className="weather-condition">Humidity: 10%</li>
+            <li className="weather-condition">Wind: 10 km/h</li>
+          </ul>
+        </div>
+
+        <p className=" weather-description">Cloudy</p>
+        <div className="row weather-today">
+          <div className="col">
+            <ul>
+              <li>
+                {" "}
+                <span className="temperature">10</span>
+                <span className="unit">°C</span>
+              </li>
+
+              <li>
+                <img src="#" alt="image" />
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
